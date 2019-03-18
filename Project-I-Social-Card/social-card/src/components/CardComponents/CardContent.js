631ad859b69c5404e737a02ce9c6a0479c4db8f5
Alan Perez <alanp9six@gmail.com>
2019-03-18 16:48:39 -0500

    completed the mvp for the assignment.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+    <div className="card-content-box">
+    <div className="card-content-info">
+    <h1 className="card-content-title">Get Started With React</h1>
+    <p className="card-content-p">React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+    <a className="link" href="www.reactjs.org">reactjs.org</a>
+    </div>
+    </div>
+    )
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+        <h1>LambdaSchool</h1>
+        <HeaderContent />
+        </div>
+    )
+}
+
+export default HeaderTitle;
