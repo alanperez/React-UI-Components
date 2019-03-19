a065ea19542622ff04be2cf555a4aca124b1d44c
Alan Perez <alanp9six@gmail.com>
2019-03-18 19:41:44 -0500

    completed the stretch for the social card section

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Header.css';
 import HeaderContent from './HeaderContent';
+import 'moment-timezone';
+import moment from 'moment';
+const time = moment();
+
+
 const HeaderTitle = () => {
     return (
         <div className="header-title">
+        <div className="header-title-info">
         <h1>LambdaSchool</h1>
+        <p className="date">@lambdaschool {time.format('Do MMM ')}</p>
+        
         <HeaderContent />
+        </div>
         </div>
     )
 }
