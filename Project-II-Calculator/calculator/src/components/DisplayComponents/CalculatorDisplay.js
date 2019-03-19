e39cd325be153ee697e76320943977aabb7b9594
Alan Perez <alanp9six@gmail.com>
2019-03-19 17:53:02 -0500

    work in progress, working on styling it so the buttons fit inside the container correctly

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,40 @@
 import React from 'react';
 import './Display.css';
+import ActionButton from '../ButtonComponents/ActionButton';
+import NumberButton from '../ButtonComponents/NumberButton';
+
+const CalculatorDisplay = () => {
+    return (
+        <div id="parent">
+        <div className="display">
+            <h1>0</h1>
+        </div>
+        <div className="buttons">
+            <div className="left-side">
+                <ActionButton styles="big white" text="Clear" />
+                <NumberButton styles="white" text="7" />
+                <NumberButton styles="white" text="8" />
+                <NumberButton styles="white" text="9" />
+                <NumberButton styles="white" text="4" />
+                <NumberButton styles="white" text="5" />
+                <NumberButton styles="white" text="6" />
+                <NumberButton styles="white" text="1" />
+                <NumberButton styles="white" text="2" />
+                <NumberButton styles="white" text="3" />
+                <NumberButton styles="white" text="0" />
+            </div>
+        <div className="right-side">
+                <ActionButton styles="red" text="/"  />
+                <ActionButton styles="red" text="X"  />
+                <ActionButton styles="red" text="-"  />
+                <ActionButton styles="red" text="+"  />
+                <ActionButton styles="red" text="="  />
+        
+        </div>
+
+        </div>
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
